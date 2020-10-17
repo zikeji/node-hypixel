d9a166128985858a9e004f56fb98846f2698f876
Zikeji <me@zikeji.com>
2020-10-17 04:39:20 -0400

    feat(docs): add new documentation using vuepress

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,20 @@
+export default ({ router }) => {
+    if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
+      var _paq = window._paq = window._paq || [];
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//analytics.zynados.com/";
+        _paq.push(['setTrackerUrl', u+'trk.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src=u+'trk.js'; s.parentNode.insertBefore(g,s);
+      })();
+  
+      router.afterEach(function (to) {
+        window._paq.push(['setDocumentTitle', document.title]);
+        window._paq.push(['setCustomUrl', to.fullPath]);
+        window._paq.push(['trackPageView']);
+      })
+    }
+  }
---
+++ docs/README.md
@@ -0,0 +1,19 @@
+---
+home: true
+heroText: hypixel-skyblock
+heroImage: /logo.svg
+tagline: NodeJS API wrapper for Hypixel's SkyBlock
+action:
+  - text: Get Started →
+    link: /guide/
+  - text: Typescript API 🖥️
+    link: /api/
+features:
+  - title: Typescript Support
+    details: Full Typescript definitions for explored API methods provides intellisense in popular IDEs.
+    link: /guide/
+---
+
+# Hello World!
+
+Pending
