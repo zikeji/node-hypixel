e8025eb5618cd2e8168c44c9602ac56bd0d9c73c
Zikeji <me@zikeji.com>
2020-10-19 03:52:17 -0400

    feat(docs): add support for code blocks in descriptions

--- docs/.vuepress/components/OpenAPI.vue
+++ docs/.vuepress/components/OpenAPI.vue
@@ -5,9 +5,38 @@
 <script>
 import SwaggerUI from "swagger-ui";
 import React from "react";
+import { Remarkable } from "remarkable";
+import { linkify } from "remarkable/linkify";
 import prism from "prismjs";
 import "prismjs/components/prism-json";
+import "prismjs/components/prism-typescript";
 import "swagger-ui/dist/swagger-ui.css";
+
+const md = new Remarkable({
+  html: true,
+  typographer: true,
+  breaks: true,
+  linkTarget: "_blank",
+  highlight: function (str, lang) {
+    return prism.highlight(str, prism.languages[lang], lang);
+  },
+}).use(linkify);
+
+md.core.ruler.disable(["replacements", "smartquotes"]);
+
+class MarkdownComponent extends React.Component {
+  render() {
+    const { source, className } = this.props;
+    if (typeof source !== "string" || !source) {
+      return null;
+    }
+    return React.createElement(
+      "div",
+      { className: `${className ? `${className} ` : ""}markdown`, dangerouslySetInnerHTML: { __html: md.render(source) } },
+      null
+    );
+  }
+}
 
 class PrismJSComponent extends React.Component {
   render() {
@@ -19,22 +48,47 @@ class PrismJSComponent extends React.Component {
     return React.createElement(
       "pre",
       {
-        className: `${this.props.className} language-json`,
+        className: this.props.className,
       },
       React.createElement(
         "code",
-        { dangerouslySetInnerHTML: { __html: code } },
+        { className: "language-json", dangerouslySetInnerHTML: { __html: code } },
         null
       )
     );
   }
 }
 
+const codeRegex = /```([a-z]*)\n([\s\S]*?)\n```/g;
 const PrismJSPlugin = function (system) {
   return {
     components: {
       highlightCode: PrismJSComponent,
+      Markdown: MarkdownComponent,
     },
+    // wrapComponents: {
+    //   Markdown: (Original) => (props) => {
+    //     console.log(md.render(props.source));
+    //     let result;
+    //     let newSource = props.source;
+    //     while ((result = codeRegex.exec(props.source)) !== null) {
+    //       const lang = result[1];
+    //       if (prism.languages[lang]) {
+    //         const code = prism.highlight(
+    //           result[2],
+    //           prism.languages[lang],
+    //           lang
+    //         );
+    //         newSource = newSource.replace(
+    //           result[0],
+    //           `<pre class="language-${lang}"><code>${code}</code></pre>`
+    //         );
+    //       }
+    //     }
+    //     props.source = newSource;
+    //     return Original(props);
+    //   },
+    // },
   };
 };
 
@@ -50,12 +104,7 @@ export default {
         syntaxHighlight: false,
         presets: [SwaggerUI.presets.apis],
         plugins: [PrismJSPlugin],
-        onComplete() {
-          const examples = document.querySelectorAll(
-            "#openapi-wrapper pre.example.microlight"
-          );
-          // console.log(examples);
-        },
+        onComplete() {},
       });
     });
   },
