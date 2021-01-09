cbd42e88905315642226533ae99c226db4e3fddb
Zikeji <me@zikeji.com>
2021-01-09 04:59:33 -0500

    docs: include full reflection in navbar title

--- docs/.vuepress/plugins/typedoc.js
+++ docs/.vuepress/plugins/typedoc.js
@@ -87,7 +87,7 @@ module.exports = function(_, { sourceDir }) {
               children: navItem.children.map((innerNavItem) => {
                 return [
                   innerNavItem.url.endsWith("index.md") ? `${innerNavItem.url.split("/").slice(0, -1).join("/")}/` : innerNavItem.url.replace(".md", ""),
-                  innerNavItem.title.split(".").slice(-1).join()
+                  innerNavItem.title
                 ];
               }),
             };
