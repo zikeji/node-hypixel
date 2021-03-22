9df8606f15d61545db0b6c263e0a596f4841f593
Zikeji <me@zikeji.com>
2021-03-21 23:47:48 -0400

    remove deno_dist [skip-ci

--- deno_dist/errors/GenericHTTPError.ts
+++
@@ -1,9 +0,0 @@
-export class GenericHTTPError extends Error {
-  /** The status code of the response */
-  public code: number;
-  constructor(code: number, message: string) {
-    super(message);
-    this.code = code;
-    Object.setPrototypeOf(this, GenericHTTPError.prototype);
-  }
-}
--- deno_dist/errors/InvalidKeyError.ts
+++
@@ -1,6 +0,0 @@
-export class InvalidKeyError extends Error {
-  constructor(message: string) {
-    super(message);
-    Object.setPrototypeOf(this, InvalidKeyError.prototype);
-  }
-}
