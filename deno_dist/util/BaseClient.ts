d169503e8a40caca35e41fd66d46912e5537e2f4
ci <ci@github.com>
2021-03-30 05:37:17 +0000

    remove deno_dist [skip-ci]

--- deno_dist/util/BaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { Components } from "../types/api.ts";
-
-export abstract class BaseClient {
-  public abstract call<T extends Components.Schemas.ApiSuccess>(
-    path: string,
-    parameters?: Record<string, string>
-  ): Promise<T & { cached?: boolean }>;
-}
