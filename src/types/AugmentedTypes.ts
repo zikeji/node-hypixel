282d0cfb4e1770f4393d6deb95267a26cb06ddf5
Zikeji <me@zikeji.com>
2024-09-15 21:40:47 -0400

    feat: more augmented types, some clean up

--- src/types/AugmentedTypes.ts
+++ src/types/AugmentedTypes.ts
@@ -17,18 +17,16 @@ export type BoostersResponse = Paths.V2Boosters.Get.Responses.$200 &
 export type CountsResponse = Paths.V2Counts.Get.Responses.$200 &
   Record<string, unknown>;
 
-export type GuildResponse = Paths.V2Guild.Get.Responses.$200 &
-  Record<string, unknown> & {
-    guild: Guild | null;
-  };
+export type GuildResponse = Paths.V2Guild.Get.Responses.$200 & {
+  guild: Guild | null;
+};
 
 export type LeaderboardsResponse = Paths.V2Leaderboards.Get.Responses.$200 &
   Record<string, unknown>;
 
-export type PlayerResponse = Paths.V2Player.Get.Responses.$200 &
-  Record<string, unknown> & {
-    player: Player;
-  };
+export type PlayerResponse = Paths.V2Player.Get.Responses.$200 & {
+  player: Player;
+};
 
 export type PunishmentStatsResponse = Paths.V2Punishmentstats.Get.Responses.$200 &
   Record<string, unknown>;
@@ -63,103 +61,143 @@ export type ResourcesVanityPetsResponse = Paths.V2ResourcesVanityPets.Get.Respon
 export type ResourcesSkyblockBingoResponse = Paths.V2ResourcesSkyblockBingo.Get.Responses.$200 &
   Record<string, unknown>;
 
-export type ResourcesSkyblockCollectionsResponse = Paths.V2ResourcesSkyblockCollections.Get.Responses.$200 &
-  Record<string, unknown> & {
-    collections: {
-      [key in
-        | "FARMING"
-        | "MINING"
-        | "COMBAT"
-        | "FORAGING"
-        | "FISHING"
-        | "RIFT"]: {
-        name: string;
-        items: {
-          [key: string]: {
-            name: string;
-            maxTiers: number;
-            tiers: {
-              tier: number;
-              amountRequired: number;
-              unlocks: string[];
-            }[];
-          };
+export type ResourcesSkyblockCollectionsResponse = Paths.V2ResourcesSkyblockCollections.Get.Responses.$200 & {
+  collections: {
+    [key in
+      | "FARMING"
+      | "MINING"
+      | "COMBAT"
+      | "FORAGING"
+      | "FISHING"
+      | "RIFT"]: {
+      name: string;
+      items: {
+        [key: string]: {
+          name: string;
+          maxTiers: number;
+          tiers: {
+            tier: number;
+            amountRequired: number;
+            unlocks: string[];
+          }[];
         };
       };
-    } & {
-      [key: string]:
-        | {
-            name: string;
-            items: {
-              [key: string]: {
-                name: string;
-                maxTiers: number;
-                tiers: {
-                  tier: number;
-                  amountRequired: number;
-                  unlocks: string[];
-                }[];
-              };
-            };
-          }
-        | undefined;
     };
+  } & {
+    [key: string]:
+      | {
+          name: string;
+          items: {
+            [key: string]: {
+              name: string;
+              maxTiers: number;
+              tiers: {
+                tier: number;
+                amountRequired: number;
+                unlocks: string[];
+              }[];
+            };
+          };
+        }
+      | undefined;
   };
+};
 
 export type ResourcesSkyblockElectionResponse = Paths.V2ResourcesSkyblockElection.Get.Responses.$200 & {
   mayor: ElectionMayor;
   current: ElectionCurrent | null;
-} & Record<string, unknown>;
+};
 
 export type ResourcesSkyblockItemsResponse = Paths.V2ResourcesSkyblockItems.Get.Responses.$200 &
   Record<string, unknown>;
 
-export type ResourcesSkyblockSkillsResponse = Paths.V2ResourcesSkyblockSkills.Get.Responses.$200 &
-  Record<string, unknown> & {
-    skills: {
-      [key: string]: {
-        name: string;
-        description: string;
-        maxLevel: number;
-        levels: {
-          level: number;
-          totalExpRequired: number;
-          unlocks: string[];
-        }[];
+export type ResourcesSkyblockSkillsResponse = Paths.V2ResourcesSkyblockSkills.Get.Responses.$200 & {
+  skills: {
+    [key: string]: {
+      name: string;
+      description: string;
+      maxLevel: number;
+      levels: {
+        level: number;
+        totalExpRequired: number;
+        unlocks: string[];
+      }[];
+    };
+  };
+};
+
+export type SkyblockNewsResponse = Paths.V2SkyblockNews.Get.Responses.$200 & {
+  items: {
+    item: {
+      material: string;
+    };
+    link: string;
+    text: string;
+    title: string;
+  }[];
+};
+
+export type SkyblockAuctionResponse = Paths.V2SkyblockAuction.Get.Responses.$200;
+
+export type SkyblockAuctionsResponse = Paths.V2SkyblockAuctions.Get.Responses.$200;
+
+export type SkyblockAuctionsEndedResponse = Paths.V2SkyblockAuctionsEnded.Get.Responses.$200 & {
+  auctions: {
+    auction_id: string;
+    seller: string;
+    seller_profile: string;
+    buyer: string;
+    buyer_profile: string;
+    timestamp: number;
+    price: number;
+    bin: boolean;
+    item_bytes: string;
+  }[];
+};
+
+export type SkyblockBazaarResponse = Paths.V2SkyblockBazaar.Get.Responses.$200 & {
+  products: {
+    /** the key is the product id, e.g. CHILI_PEPPER */
+    [product_id: string]: {
+      product_id: string;
+      sell_summary: {
+        amount: number;
+        pricePerUnit: number;
+        orders: number;
+      }[];
+      buy_summary: {
+        amount: number;
+        pricePerUnit: number;
+        orders: number;
+      }[];
+      quick_status: {
+        productId: string;
+        sellPrice: number;
+        sellVolume: number;
+        sellMovingWeek: number;
+        sellOrders: number;
+        buyPrice: number;
+        buyVolume: number;
+        buyMovingWeek: number;
+        buyOrders: number;
       };
     };
   };
-
-export type SkyblockNewsResponse = Paths.V2SkyblockNews.Get.Responses.$200 &
-  Record<string, unknown>;
-
-export type SkyblockAuctionResponse = Paths.V2SkyblockAuction.Get.Responses.$200 &
-  Record<string, unknown>;
-
-export type SkyblockAuctionsResponse = Paths.V2SkyblockAuctions.Get.Responses.$200 &
-  Record<string, unknown>;
-
-export type SkyblockAuctionsEndedResponse = Paths.V2SkyblockAuctionsEnded.Get.Responses.$200 &
-  Record<string, unknown>;
-
-export type SkyblockBazaarResponse = Paths.V2SkyblockBazaar.Get.Responses.$200 &
-  Record<string, unknown>;
+};
 
 export type SkyblockProfileResponse = Omit<
   Paths.V2SkyblockProfile.Get.Responses.$200,
   "profile"
-> &
-  Record<string, unknown> & {
-    profile: SkyBlockProfile;
-  };
+> & {
+  profile: SkyBlockProfile;
+};
 
 export type SkyblockProfilesResponse = Omit<
   Paths.V2SkyblockProfiles.Get.Responses.$200,
   "profiles"
-> &
-  Record<string, unknown> & {
-    profiles: SkyBlockProfile[];
-  };
+> & {
+  profiles: SkyBlockProfile[];
+};
 
 export type SkyblockMuseumResponse = Paths.V2SkyblockMuseum.Get.Responses.$200 &
   Record<string, unknown>;
